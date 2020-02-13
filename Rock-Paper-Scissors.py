70c71462ea39925f5d1d2570348ea5976a6c5c8b
tsyvic2021307 <60993164+tsyvic2021307@users.noreply.github.com>
2020-02-13 14:10:42 +1300

    Update Rock-Paper-Scissors.py
    Updated game

--- Rock-Paper-Scissors.py
+++ Rock-Paper-Scissors.py
@@ -1,12 +1,13 @@
-# Ex 1
+# Ex 2
 # initialising game
 from random import randint
 import getpass
 
 names = ["",""]
-starter = randint(0, 1)
+points = [0,0]
 second = None
 choices = ["Rock", "Paper", "Scissors"]
+starter = randint(0, 1)
 # set second to alt of starter
 if starter == 1:
     second = 0
@@ -31,8 +32,10 @@ def checking(): # check the result of choices
     return result
 
 def congrats(player):
-    print("Congrats %s, you've won the match!" %(names[player]))
+    print("%s, you've won this round!" %(names[player]))
 
+def winner(player):
+    print("Congratulations %s, you won the game!!" %(names[player]))
 
 # get some user inputs / main game
 while len(names[0]) < 2:
@@ -40,21 +43,43 @@ while len(names[0]) < 2:
 while len(names[1]) < 2:
     names[1] = input("Player B please enter your first name: ")
 
-print("------------------------------------------\r\n%s has been randomly chosen to start the game." %(names[starter]))
+rounds = int(input("How many rounds do you wish to play? "))
+print("------------------------------------------\r\n%s has been randomly chosen to start each round of the game." %(names[starter]))
+currentRound = 1
 
-startersChoice = weaponChoice(starter)
-secondsChoice = weaponChoice(second)
+while currentRound < rounds+1:
+    startersChoice = weaponChoice(starter)
+    secondsChoice = weaponChoice(second)
 
-print("------------------------------------------\r\nResults: %s (%s) vs %s (%s)" %(names[0], choices[startersChoice-1], names[1], choices[secondsChoice-1]))
+    print("------------------------------------------\r\nRound %s Results: %s (%s) vs %s (%s)" %(currentRound, names[0], choices[startersChoice-1], names[1], choices[secondsChoice-1]))
 
-theResult = checking()
-if theResult == 0:
-    print("The game was a tie!!")
-elif theResult == 1:
-    congrats(0)
-elif theResult == 2:
-    congrats(1)
-elif theResult == 3:
-    print("This shouldn't happen...")
+    theResult = checking()
+    if theResult == 0:
+        print("This round was a tie!!")
+    elif theResult == 1:
+        congrats(0)
+        points[0] += 1
+    elif theResult == 2:
+        congrats(1)
+        points[1] += 1
+    elif theResult == 3:
+        print("This shouldn't happen...")
+    else:
+        print("This REALLY shouldn't happen...")
+    
+    print("------------------------------------------\r\nOverall Points:\r\n%s - %s\r\n%s - %s" %(names[0], points[0], names[1], points[1]))
+    
+    currentRound += 1
+    if currentRound <= rounds:
+        getpass.getpass("Press Enter to continue") # if user types anything it won't be visible
+
+print("------------------------------------------")
+if points[0] == points[1]:
+    print("The game was a tie!")
+elif points[0] > points[1]:
+    winner(0)
+elif points[0] < points[1]:
+    winner(1)
 else:
-    print("This REALLY shouldn't happen...")
+    print("Shouldn't happen")
+print("------------------------------------------")
