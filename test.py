1a5b0dd43b35cf615bf3276f7c7b87378febc99f
tsyvic2021307 <60993164+tsyvic2021307@users.noreply.github.com>
2020-03-03 22:32:38 +1300

    Added testing

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
 import pytest
-import rps_modules
+import rps_modules as rps
 import getpass
 
 # Weapons Testing
-@pytest.mark.set1
+#@pytest.mark.set1
 def test_weapons1():
     assert rps.weaponChoice("Player")[1] == "------------------------------------------\r\nPlayer, please choose your weapon:\r\n1. Rock\r\n2. Paper\r\n3. Scissors\r\nEnter your choice (use the appropriate number): "
 
 # Check Testing
-#@pytest.mark.parametrize("in1, in2, out", [(2, 2, 0), (2, 1, 1), (1, 2, 2)])
+@pytest.mark.parametrize("in1, in2, out", [(2, 2, 0), (2, 1, 1), (1, 2, 2)])
 def test_checkingExpected2(in1, in2, out):
     assert rps.checking(in1, in2) == out
 
@@ -19,6 +19,6 @@ def test_checkingUnexpected1():
 def test_congrats():
     assert rps.congrats("Player") == print("Player, you've won this round!")
 
-# Winner Testing
-def test_winner():
-    assert rps.congrats("Player") == print("Congratulations Player, you won the game!!
+# # Winner Testing
+# def test_winner():
+#     assert rps.congrats("Player") == print("Congratulations Player, you won the game!!
